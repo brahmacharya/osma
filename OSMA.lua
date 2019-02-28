c09b1199d88ae5d0509f1e0287524e17255408da
brahmacharya <43069175+brahmacharya@users.noreply.github.com>
2019-02-28 18:39:08 +0300

    Create OSMA.lua

---
+++ OSMA.lua
@@ -0,0 +1,22 @@
+Settings=
+{
+  Name="Example2",
+  period=5,
+  line=
+  {
+    {
+      Name="MA",
+      Color=RGB(255,0,0),
+      Type=TYPE_LINE,
+      Width=2
+    }
+  }
+}
+
+function Init()
+  return 1
+end
+
+function OnCalculate(index)
+  return nil
+end
